37270c822f85248b22fb7c56052ad89e7b57bdc8
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2025-07-27 17:56:04 +0530

    Update design.sv

--- 4-bit Adder_Subtract/design.sv
+++ 4-bit Adder_Subtract/design.sv
@@ -7,3 +7,26 @@ module FA (
   
 endmodule
 
+module addsub (
+  input [3:0] A, B,
+  input Cin,
+  output [3:0] sum,
+  output cout
+);
+  
+  wire [3:0] B_ = B ^ {4{Cin}};
+  wire [4:0] C;
+  
+  assign C[0] = Cin;
+  
+  genvar i;
+  
+  generate 
+    for (i = 0; i < 4; i++) begin
+      FA fa(A[i], B_[i], C[i], sum[i], C[i+1]);
+    end
+  endgenerate
+  
+  assign cout = C[4];
+  
+endmodule  
